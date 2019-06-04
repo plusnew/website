ae579900631345b048b5dd734c34d9327b5ae6b3
plusgut <github@webentwickler2-0.de>
2019-06-04 15:31:44 +0200

    [route] added some more routes

---
+++ src/Core/components/Content/components/ImpressumRoute/components/ImpressumContent/index.tsx
@@ -0,0 +1,9 @@
+import plusnew, { component } from 'plusnew';
+
+export default component(
+  __dirname,
+  () =>
+    <div>
+      <p>plusnew is made by Carlo Jeske</p>
+    </div>,
+);
---
+++ src/Core/components/Content/components/ImpressumRoute/index.tsx
@@ -0,0 +1,23 @@
+import { createRoute } from '@plusnew/router';
+import plusnew, { Async, component } from 'plusnew';
+import Error from 'shared/Components/Error';
+import Loader from 'shared/Components/Loader';
+
+const Container = component(
+  __dirname,
+  () =>
+    <Async
+      pendingIndicator={<div><Loader /></div>}
+    >{() =>
+      // tslint:disable-next-line: space-in-parens
+      import(/* webpackChunkName: "site/impressum" */ './components/ImpressumContent')
+        .then(module => <module.default />)
+        .catch(() => <Error message="Could not load" />)
+    }</Async>,
+);
+
+export default createRoute(
+  ['impressum'],
+  {},
+  Container,
+);
